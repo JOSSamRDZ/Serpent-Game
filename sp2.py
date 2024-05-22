5da345599cc17952c5356dee656ad136223a24b8
Sam <loboabel.cronox@gmail.com>
2024-05-22 15:26:47 -0600

    Se creo la funcion mov para el set de movimientos del elemenot square

--- sp2.py
+++ sp2.py
@@ -1,29 +1,43 @@
 import turtle
+#Usamos el modulo time para realentizar la velocidad de nuestro elemento
+import time #La función time.sleep(segundos) pausa la ejecución del programa durante el número de segundos especificado.
+posponer = 0.1
 #Configuración de la ventana
 window =  turtle.Screen()
 window.title ("Serpent Game")
 window.bgcolor("gray")
 window.setup(width=600, height=600)
-window.tracer()
+window.tracer(0)#Mejora las animaciones, según...
 
 #Cabeza de la serpiente
 cabeza = turtle.Turtle()
 cabeza.speed(0)
 cabeza.shape("square")
-cabeza.penup()
-cabeza.goto(0,0)
-cabeza.direction = "up"
+cabeza.penup()#Aunque movamos el elemeno no deja ningun rastro
+cabeza.goto(0,0)#Le da una posición al elemento
+cabeza.direction = "right"
 
 #Funciones
 def mov():
     if cabeza.direction == "up":
-        y = cabeza.ycor()
-        cabeza.sety(y+2)
+        y = cabeza.ycor() #Obtiene la cordenada del elemnto
+        cabeza.sety(y+10)
+    if cabeza.direction == "down":
+        y = cabeza.ycor() 
+        cabeza.sety(y-10)
+    if cabeza.direction == "left":
+        x = cabeza.xcor() 
+        cabeza.setx(x-10)
+    if cabeza.direction == "right":
+        x = cabeza.xcor() #Obtiene la cordenada del elemnto
+        cabeza.setx(x+10)
+        
 
 
 #Crear un bucle principal o while loop
 while True:
-    window.update()
+    window.update() #CUANDO CREAMOS UN JUEGO TEMEMOS QUE TENER UN BULE PRINCIPAL
     mov()
+    time.sleep(posponer) #Aquí especificamente realentizamos el tiempo del movimiento de nuestro elemento con la función time.sleep
 #Mantener la ventana abierta
 turtle.done()
