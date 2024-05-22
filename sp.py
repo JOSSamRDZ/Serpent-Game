513158902ec3a5e623cf399c691701fb4a99fd0d
Sam <loboabel.cronox@gmail.com>
2024-05-22 07:13:14 -0600

    Este es el primer commit, en este proyecto crearemos el juego de la serpiente los avances hastaahora son lo siguientes:

---
+++ sp.py
@@ -0,0 +1,10 @@
+import turtle
+pluma = turtle.Turtle()
+pluma.speed(1)
+for i in range (4):
+    pluma.forward(100)
+    pluma.left(90)
+
+
+# Mantener la ventana abierta
+turtle.done()
---
+++ sp2.py
@@ -0,0 +1,29 @@
+import turtle
+#Configuración de la ventana
+window =  turtle.Screen()
+window.title ("Serpent Game")
+window.bgcolor("gray")
+window.setup(width=600, height=600)
+window.tracer()#
+
+#Cabeza de la serpiente
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = "up"
+
+#Funciones
+def mov():
+    if cabeza.direction == "up":
+        y = cabeza.ycor()
+        cabeza.sety(y+2)
+
+
+#Crear un bucle principal o while loop
+while True:
+    window.update()
+    mov()
+#Mantener la ventana abierta
+turtle.done()
